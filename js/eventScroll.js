9091124e0e0bd7fbb9d02dc9fe350cd13d9af5cb
so-suke <attoutekimasshu@gmail.com>
2019-05-20 20:57:19 +0900

    対象が画面内にちょっと入っただけで実行されてしまうのを防ぐ

--- js/eventScroll.js
+++ js/eventScroll.js
@@ -9,7 +9,10 @@ import { animateIfIncludeWindow } from './anima';
  */
 const setEventScrolledAnimation = () => {
   window.addEventListener("scroll", function () {
-    const scrolledPosition = $window.scrollTop() + $window.height();
+    const windowHeight = $window.height();
+    // 少しゆったり目の実行とするための調整値(画面下から画面の7分の1の高さでの実行目的)
+    const scrolledPositionAdjust = (windowHeight / 7);
+    const scrolledPosition = $window.scrollTop() + windowHeight - scrolledPositionAdjust;
 
     // イベントを設定
     $firedWithinScroll.each((idx, element) => {
